88eb8d6680163dd7936e9fff935f63447cd379d6
Thanapat Nawang <thanapattt.m@gmail.com>
2025-02-09 16:21:31 +0700

    Add: font

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ export default function Home() {
   return (
     <div className="bg-slate-100">
       <h1 className="text-xl text-blue-700 font-bold">Hello kub</h1>
+      <p className="font-bold">ว่าไง</p>
     </div>
   );
 }
