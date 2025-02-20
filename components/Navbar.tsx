7a078eb2fb5a6b1eaa8e8c5b6087e8f1b49bc217
Thanapat Nawang <thanapattt.m@gmail.com>
2025-02-20 21:47:21 +0700

    Add: app name

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='bg-blue-200 h-10 px-5 py-3 flex flex-row items-center'>
-      Navbar
+    <div className='fixed top-0 z-30 h-10 px-5 py-3 flex flex-row items-center'>
+      <p className='text-lg font-semibold text-textb'>WatGuide</p>
     </div>
   )
 }
