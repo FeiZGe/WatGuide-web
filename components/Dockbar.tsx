19f9004376953703c967e9d50007837b760d188f
Thanapat Nawang <thanapattt.m@gmail.com>
2025-02-22 23:15:40 +0700

    Edit: Dockbar Size#1

--- components/Dockbar.tsx
+++ components/Dockbar.tsx
@@ -4,8 +4,8 @@ import { faHome, faBook, faMapLocationDot, faTicket, faWandMagicSparkles } from 
 
 const Dockbar = () => {
   return (
-    <div className="fixed bottom-4 left-1/2 -translate-x-1/2 z-40 bg-slate-300 bg-opacity-30 backdrop-blur-md h-12 rounded-lg shadow-lg">
-      <div className="flex flex-row gap-2 py-1 px-1.5 justify-center items-center h-full">
+    <div className="fixed bottom-4 left-1/2 -translate-x-1/2 z-40 bg-slate-300 bg-opacity-30 backdrop-blur-md w-72 h-16 rounded-lg shadow-lg">
+      <div className="flex flex-row gap-2 p-2 justify-between items-center h-full">
         <Menulink icon={faHome} />
         <Menulink icon={faBook} />
         <Menulink icon={faWandMagicSparkles} bg="bg-gradient-to-br from-amber-500 to-red-500 text-slate-200" />
--- components/Menulink.tsx
+++ components/Menulink.tsx
@@ -14,11 +14,11 @@ const Menulink = ({ path = "#", icon, bg }: menuProps) => {
     <Link 
         href={path}
         className={clsx(
-            "p-2.5 rounded-md hover:text-slate-500 transition-all duration-300 ease-in-out hover:scale-110",
+            "p-2 w-full h-full flex justify-center items-center rounded-md hover:text-slate-500 transition-all duration-300 ease-in-out hover:scale-110",
             bg ? bg : "bg-slate-100 hover:bg-slate-300/50 bg-opacity-10 backdrop-blur-sm text-slate-700"
         )}
     >
-        <FontAwesomeIcon icon={icon} className="w-4 h-4" />
+        <FontAwesomeIcon icon={icon} className="w-6 h-6" />
     </Link>
   )
 }
