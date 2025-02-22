61672f62438fc673cefb4691ac92e2e2cf7fdbe4
Thanapat Nawang <thanapattt.m@gmail.com>
2025-02-22 23:03:44 +0700

    Create: menu button

---
+++ components/Menulink.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { IconDefinition } from "@fortawesome/fontawesome-svg-core";
+import clsx from "clsx";
+
+interface menuProps {
+    path?: string;
+    icon: IconDefinition;
+    bg?: string; 
+}
+
+const Menulink = ({ path = "#", icon, bg }: menuProps) => {
+  return (
+    <Link 
+        href={path}
+        className={clsx(
+            "p-2.5 rounded-md hover:text-slate-500 transition-all duration-300 ease-in-out hover:scale-110",
+            bg ? bg : "bg-slate-100 hover:bg-slate-300/50 bg-opacity-10 backdrop-blur-sm text-slate-700"
+        )}
+    >
+        <FontAwesomeIcon icon={icon} className="w-4 h-4" />
+    </Link>
+  )
+}
+
+export default Menulink
