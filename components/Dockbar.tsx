a31957e64bb95708705592475adaf6b91a92543b
Thanapat Nawang <thanapattt.m@gmail.com>
2025-02-23 20:27:20 +0700

    Add: link

--- components/Dockbar.tsx
+++ components/Dockbar.tsx
@@ -6,11 +6,11 @@ const Dockbar = () => {
   return (
     <div className="fixed bottom-4 left-1/2 -translate-x-1/2 z-40 bg-slate-300 bg-opacity-30 backdrop-blur-md max-w-80 w-4/5 h-16 rounded-lg shadow-lg">
       <div className="flex flex-row gap-2 p-2 justify-between items-center h-full">
-        <Menulink icon={faHome} />
-        <Menulink icon={faBook} />
-        <Menulink icon={faWandMagicSparkles} bg="bg-gradient-to-br from-amber-500 to-red-500 text-slate-200" />
-        <Menulink icon={faMapLocationDot} />
-        <Menulink icon={faTicket} />
+        <Menulink icon={faHome} path="/" />
+        <Menulink icon={faBook} path="/info" />
+        <Menulink icon={faWandMagicSparkles} path="/destiny" bg="bg-gradient-to-br from-amber-500 to-red-500 text-slate-200" />
+        <Menulink icon={faMapLocationDot} path="/map" />
+        <Menulink icon={faTicket} path="/event" />
       </div>
     </div>
   )
