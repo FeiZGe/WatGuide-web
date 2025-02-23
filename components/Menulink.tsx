2c6a9140839f4238140245ef7cc511d61402fd45
Thanapat Nawang <thanapattt.m@gmail.com>
2025-02-23 20:48:02 +0700

    Edit: animate

--- components/Menulink.tsx
+++ components/Menulink.tsx
@@ -1,26 +1,35 @@
-import Link from "next/link"
+"use client";
+
+import Link from "next/link";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { IconDefinition } from "@fortawesome/fontawesome-svg-core";
 import clsx from "clsx";
+import { motion } from "motion/react";
 
 interface menuProps {
-    path?: string;
-    icon: IconDefinition;
-    bg?: string; 
+  path?: string;
+  icon: IconDefinition;
+  bg?: string;
 }
 
 const Menulink = ({ path = "#", icon, bg }: menuProps) => {
   return (
-    <Link 
+    <motion.div
+      whileHover={{ scale: 1.1 }}
+      whileTap={{ scale: 0.9 }}
+      className="w-full h-full"
+    >
+      <Link
         href={path}
         className={clsx(
-            "p-2 w-full h-full flex justify-center items-center rounded-md hover:text-slate-500 transition-all duration-300 ease-in-out hover:scale-110",
-            bg ? bg : "bg-slate-100 hover:bg-slate-300/50 bg-opacity-10 backdrop-blur-sm text-slate-700"
+          "p-2 w-full h-full flex justify-center items-center rounded-md",
+          bg ? bg : "bg-slate-100 bg-opacity-10 backdrop-blur-sm text-slate-700"
         )}
-    >
+      >
         <FontAwesomeIcon icon={icon} className="w-5 h-5" />
-    </Link>
-  )
-}
+      </Link>
+    </motion.div>
+  );
+};
 
-export default Menulink
+export default Menulink;
