e22f338ed9f09762ae4c137fb8c2b4ffd348d5e4
Thanapat Nawang <thanapattt.m@gmail.com>
2025-02-23 21:53:22 +0700

    Update: design#1

--- app/destiny/page.tsx
+++ app/destiny/page.tsx
@@ -1,9 +1,51 @@
-const Destiny = () => {
-  return (
-    <div>
-      Destiny
-    </div>
-  )
-}
+"use client";
+import { motion } from "motion/react";
+import { useRef, useState } from "react";
 
-export default Destiny
+const Destiny = () => {
+    const constraintsRef = useRef(null)
+    const [text, setText] = useState("");
+    const [isShaking, setIsShaking] = useState(false);
+    
+    const handleClick = () => {
+        if (isShaking) return; // ป้องกันการกดซ้ำ
+        setIsShaking(true);
+
+        setTimeout(() => {
+            setText("hello");
+            setIsShaking(false);
+        }, 500); // จบ animation แล้วเปลี่ยนข้อความ
+    };
+
+    return (
+      <div className="bg-red-300 h-dvh flex flex-col items-center justify-center">
+        <section className="px-8 max-w-xl mx-auto h-4/5 w-full">
+          <div className="flex flex-col justify-center items-center h-full w-full">
+            <h1 className="text-4xl font-bold">เซียมซี</h1>
+            <p className="opacity-70 mb-12">อธิษฐานก่อนเขย่า</p>
+            <p>{text}</p>
+
+            {/* เซียมซี Zone */}
+            <div ref={constraintsRef} className="relative w-44 h-44 md:w-52 md:h-52">
+              <motion.button 
+                onClick={handleClick}
+                className="w-full h-full bg-slate-200 rounded-full flex flex-col justify-center items-center"
+              >
+                <motion.img
+                  drag
+                  dragConstraints={constraintsRef}
+                  className="w-32 h-32"
+                  src="/image/destiny/cup.png"
+                  alt="เซียมซี"
+                  animate={isShaking ? { x: [0, -10, 10, -10, 10, 0] } : {}}
+                  transition={{ duration: 0.5 }}
+                />
+              </motion.button>
+            </div>
+          </div>
+        </section>
+      </div>
+    );
+  }
+  
+  export default Destiny;  
