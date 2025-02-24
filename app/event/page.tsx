47fd1ccd6faf87b12756941520a04c5a474bdd79
Thanapat Nawang <thanapattt.m@gmail.com>
2025-02-24 17:35:01 +0700

    Update: content

--- app/event/page.tsx
+++ app/event/page.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+"use client";
+
+import NCard from "@/components/NCard"
+import prays from "@/utils/prayData"
 
 const Event = () => {
   return (
-    <div>
-      Event
+    <div className="columns-1 sm:columns-2 gap-3 space-y-3">
+      {prays.map((num) => (
+        <NCard 
+          key={num.id} 
+          img={`/image/pray/${num.id}.jpg`}
+          title={num.name} 
+          detail={num.description}
+          />
+      ))}
     </div>
   )
 }
