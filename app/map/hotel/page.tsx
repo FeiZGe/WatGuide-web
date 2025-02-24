9953797aab620bcf95ad6ee71a396c3ee7b32709
Thanapat Nawang <thanapattt.m@gmail.com>
2025-02-24 15:12:57 +0700

    Add: hotel data

--- app/map/hotel/page.tsx
+++ app/map/hotel/page.tsx
@@ -1,12 +1,18 @@
 import Card from '@/components/Card'
-import React from 'react'
+import hotels from "@/utils/hotelData"
 
 const Hotel = () => {
   return (
     <div className="grid sm:grid-cols-2 grid-cols-1 gap-3">
-      <Card img="/image/test.png" path="/map/hotel" title="Hello" detail="ahahahhaasa" />
-      <Card img="/image/test.png" path="/map/hotel" title="Hello" detail="lorsakmdmskmxksamk maskdms akdmmxak msax " />
-      <Card img="/image/test.png" path="/map/hotel" title="Hello" detail="ahahahhaasa" />
+      {hotels.map((num) => (
+        <Card 
+          key={num.id} 
+          img="/image/test.png" 
+          path={`/map/hotel/${num.id}`} 
+          title={num.name_th} 
+          detail={num.price}
+          rate={num.rating} />
+      ))}
     </div>
   )
 }
