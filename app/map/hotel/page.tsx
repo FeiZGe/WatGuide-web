552ef74fee0c659a6c9c93e2c3258a069667cf7d
Thanapat Nawang <thanapattt.m@gmail.com>
2025-02-24 14:42:43 +0700

    Add: hotel design

--- app/map/hotel/page.tsx
+++ app/map/hotel/page.tsx
@@ -1,9 +1,12 @@
+import Card from '@/components/Card'
 import React from 'react'
 
 const Hotel = () => {
   return (
-    <div>
-      Hotel
+    <div className="grid sm:grid-cols-2 grid-cols-1 gap-3">
+      <Card img="/image/test.png" path="/map/hotel" title="Hello" detail="ahahahhaasa" />
+      <Card img="/image/test.png" path="/map/hotel" title="Hello" detail="lorsakmdmskmxksamk maskdms akdmmxak msax " />
+      <Card img="/image/test.png" path="/map/hotel" title="Hello" detail="ahahahhaasa" />
     </div>
   )
 }
