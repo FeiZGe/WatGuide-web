c36fc920eaa756c8ec092ef9b2ca7ca490ac2530
Thanapat Nawang <thanapattt.m@gmail.com>
2025-02-24 15:20:08 +0700

    Add: menu

--- app/map/restaurant/page.tsx
+++ app/map/restaurant/page.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import Card from '@/components/Card'
+import restaurants from '@/utils/resData'
 
 const Restaurant = () => {
   return (
-    <div>
-      Restaurant
+    <div className="grid sm:grid-cols-2 grid-cols-1 gap-3">
+      {restaurants.map((num) => (
+        <Card 
+          key={num.id} 
+          img="/image/test.png" 
+          path={`/map/hotel/${num.id}`} 
+          title={num.name} 
+          detail={num.typefood}
+          rate={num.rating} />
+      ))}
     </div>
   )
 }
