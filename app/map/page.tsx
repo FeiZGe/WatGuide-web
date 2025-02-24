bca4c0488fe0fc9ed9c62863a8ede654c832a6b6
Thanapat Nawang <thanapattt.m@gmail.com>
2025-02-24 13:20:49 +0700

    Edit: Map page#1

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -2,7 +2,10 @@
 const Map = () => {
   return (
     <div>
-      Map
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis earum consequuntur nostrum at amet corporis, velit minus vitae inventore aperiam nihil maiores unde veritatis architecto cum doloremque placeat, vel possimus nam cumque, tempore nulla veniam? Voluptas porro quibusdam reiciendis eos.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis earum consequuntur nostrum at amet corporis, velit minus vitae inventore aperiam nihil maiores unde veritatis architecto cum doloremque placeat, vel possimus nam cumque, tempore nulla veniam? Voluptas porro quibusdam reiciendis eos.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis earum consequuntur nostrum at amet corporis, velit minus vitae inventore aperiam nihil maiores unde veritatis architecto cum doloremque placeat, vel possimus nam cumque, tempore nulla veniam? Voluptas porro quibusdam reiciendis eos.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis earum consequuntur nostrum at amet corporis, velit minus vitae inventore aperiam nihil maiores unde veritatis architecto cum doloremque placeat, vel possimus nam cumque, tempore nulla veniam? Voluptas porro quibusdam reiciendis eos.</p>
     </div>
   )
 }
