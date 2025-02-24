2138b9f327b84d8ea6483fb0854bf8f4c53a63c1
Thanapat Nawang <thanapattt.m@gmail.com>
2025-02-24 13:26:36 +0700

    Add: selected path

--- components/SmallBtn.tsx
+++ components/SmallBtn.tsx
@@ -4,27 +4,31 @@ import { IconDefinition } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { motion } from "motion/react";
 import Link from "next/link";
+import { usePathname } from "next/navigation"; // ใช้เพื่อเช็ค path ปัจจุบัน
+import clsx from "clsx";
 
 interface SmallBtnProps {
-    icon: IconDefinition;
-    txt: string;
-    path: string;
+  icon: IconDefinition;
+  txt: string;
+  path: string;
 }
 
 const SmallBtn = ({ icon, txt, path }: SmallBtnProps) => {
+  const pathname = usePathname(); // ดึง path ปัจจุบัน
+
   return (
     <motion.div
-        className="py-1 px-3 bg-slate-100/70 rounded-full shadow-sm"
+      className={clsx(
+        "py-1 px-3 rounded-full shadow-sm transition-colors",
+        pathname === path ? "bg-amber-500/30" : "bg-slate-100/70"
+      )}
     >
-      <Link
-        href={path}
-        className="flex flex-row gap-2 items-center text-sm"
-      >
+      <Link href={path} className="flex flex-row gap-2 items-center text-sm">
         <FontAwesomeIcon icon={icon} />
         <h3>{txt}</h3>
       </Link>
     </motion.div>
-  )
-}
+  );
+};
 
-export default SmallBtn
+export default SmallBtn;
